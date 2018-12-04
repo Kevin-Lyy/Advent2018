ee27cf3a27bdb44f285e7bff44aa60db4ebf0bfa
Kevinlyy <batmanthelegendly@gmail.com>
2018-12-04 13:04:32 -0500

    py

---
+++ 01/pythonday1.py
@@ -0,0 +1,9 @@
+filename = "text.txt"
+file = open(filename,"r")
+sum = 0
+for line in file:
+    if line[0] == '+':
+        sum += int(line[1:])
+    if line[0] == '-':
+        sum -= int(line[1:])
+print (sum)
